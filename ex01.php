432bbc4ccc9832fd2c2d8dc832688629e38f716b
Gabriel Block <gabriel.block@imie.fr>
2016-05-03 15:41:32 +0200

    Initial Commit

---
+++ ex01.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Bases PHP</title>
+  </head>
+  <body>
+    <?php include("header.html"); ?>
+    <main>
+      <h2>Exercices</h2>
+      <?php include("nav.html"); ?>
+      <h3>Exercice 1 :</h3>
+      <?php
+
+      ?>
+    </main>
+    <?php include("footer.html"); ?>
+  </body>
+</html>
