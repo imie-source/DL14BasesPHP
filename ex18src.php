29658cfbf04f792c0b40d1fd1597012b2285de85
Gabriel Block <gabriel.block@imie.fr>
2016-05-09 15:05:13 +0200

    ex 18, 19, 20

--- ex18src.php
+++ ex18src.php
@@ -1,1 +1,12 @@
 <?php
+
+$nb = 6;
+$str = "* ";
+for($ii = 1; $ii <= $nb; $ii++){
+  for($jj = $nb-$ii; $jj > 0; $jj--){
+    echo "&nbsp;";
+  }
+  echo $str;
+  $str .= "* ";
+  echo "<br/>";
+}
--- ex19src.php
+++ ex19src.php
@@ -1,1 +1,14 @@
 <?php
+
+for($ii = 2; $ii <= 100; $ii++){
+  $prime = true; // flag
+  for($jj = 2; $jj <= ceil($ii/2); $jj++){
+    if($ii % $jj === 0){
+      $prime = false;
+      break;
+    }
+  }
+  if($prime){
+    echo "$ii est un nombre premier.<br/>";
+  }
+}
