f3587fb657144f0d9ee89abd118bcae3a3f1e469
Gabriel Block <gabriel.block@imie.fr>
2016-05-09 12:03:37 +0200

    bootstrap fix

--- ex.php
+++ ex.php
@@ -16,15 +16,25 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" media="screen" title="no title" charset="utf-8">
   </head>
   <body>
-    <?php include("header.html"); ?>
-    <?php include("nav.php"); ?>
-    <main>
-      <?php
-        echo "<h3>Exercice $ex</h3>";
-        getStatement($ex);
-        showExercise($ex);
-      ?>
-    </main>
-    <?php include("footer.html"); ?>
+    <div class="container">
+      <div class="row">
+        <?php include("header.html"); ?>
+      </div>
+      <div class="row">
+        <div class="col-sm-4">
+          <?php include("nav.php"); ?>
+        </div>
+        <main class="col-sm-8">
+          <?php
+            echo "<h3>Exercice $ex</h3>";
+            getStatement($ex);
+            showExercise($ex);
+          ?>
+        </main>
+      </div>
+      <div class="row">
+        <?php include("footer.html") ?>
+      </div>
+    </div>
   </body>
 </html>
--- index.php
+++ index.php
@@ -7,11 +7,21 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" media="screen" title="no title" charset="utf-8">
   </head>
   <body>
-    <?php include("header.html"); ?>
-    <?php include("nav.php"); ?>
-    <main>
-      <h1>Exercices de bases de PHP</h1>
-    </main>
-    <?php include("footer.html") ?>
+    <div class="container">
+      <div class="row">
+        <?php include("header.html"); ?>
+      </div>
+      <div class="row">
+        <div class="col-sm-4">
+          <?php include("nav.php"); ?>
+        </div>
+        <main class="col-sm-8">
+          <h1>Exercices de bases de PHP</h1>
+        </main>
+      </div>
+      <div class="row">
+        <?php include("footer.html") ?>
+      </div>
+    </div>
   </body>
 </html>
